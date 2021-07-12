64c059bfcd3c0bdcbabb6377d6e6e07e467c5fb8
김태현 <kth@elonsoft.co.kr>
2021-07-12 18:23:16 +0900

    license module init commit

--- app/license-test-app/src/test/java/com/license/test/app/LicenseTestAppApplicationTests.java
+++ app/license-test-app/src/test/java/com/license/test/app/LicenseTestAppApplicationTests.java
@@ -1,12 +1,9 @@
 package com.license.test.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LicenseTestAppApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
